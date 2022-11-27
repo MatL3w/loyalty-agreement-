15577a4899f70b5def79992b6f03f215233ee61a
Lecislav <lecislav@protonmail.com>
2022-11-27 19:49:05 +0100

    preparing files

---
+++ app.js
@@ -0,0 +1,17 @@
+//***MODULES>>>> */
+
+//***MODULES <<<<*/
+
+//***VARAIABLES>>>> */
+
+//***VARAIABLES <<<<*/
+
+//***CORE>>>> */
+
+//***CORE<<<<*/
+
+//***FUNCTIONS>>>> */
+
+setInterval(() => {
+  console.log("lol");
+}, 1000); //repetition time = 5 sec
