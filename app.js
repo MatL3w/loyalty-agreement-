aca57df7d993bc1fa76f3a02225299811a33f6a1
Lecislav <lecislav@protonmail.com>
2022-11-27 20:39:29 +0100

    draft loyalty agreement

--- app.js
+++ app.js
@@ -3,15 +3,24 @@
 //***MODULES <<<<*/
 
 //***VARAIABLES>>>> */
-
+const LoyaltyAgreement1_endDate = new Date("2028-08-22T24:00:00.000Z");
+const LoyaltyAgreement1_startDate = new Date("2013-08-22T24:00:00.000Z");
+const LoyaltyAgreement1_plnAmount = 94541;
+let LoyaltyAgreement1_plnRest = 0;
+let time = new Date().getTime();
 //***VARAIABLES <<<<*/
 
 //***CORE>>>> */
 
+setInterval(() => {
+    const tDif=LoyaltyAgreement1_endDate-LoyaltyAgreement1_startDate;
+    const dif = LoyaltyAgreement1_endDate - new Date().getTime();
+    LoyaltyAgreement1_plnRest =`Ilość PLN do zwrotu: ${(dif/tDif)*LoyaltyAgreement1_plnAmount}`;
+  console.log(LoyaltyAgreement1_plnRest);
+
+
+}, 1000);
 //***CORE<<<<*/
 
 //***FUNCTIONS>>>> */
-
-setInterval(() => {
-  console.log("lol");
-}, 1000); //repetition time = 5 sec
+ //repetition time = 5 sec
